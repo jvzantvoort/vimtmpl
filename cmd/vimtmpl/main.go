dfa51d4efd061a0a60165a7a73d2159e500014df
John van Zantvoort <john@vanzantvoort.org>
2022-11-06 20:04:21 +0100

    fixed permission creation

--- cmd/vimtmpl/main.go
+++ cmd/vimtmpl/main.go
@@ -1,29 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"os"
 	"strings"
+	"text/template"
 
 	"github.com/jvzantvoort/vimtmpl/config"
 	"github.com/jvzantvoort/vimtmpl/templates"
 	log "github.com/sirupsen/logrus"
 )
-
-type Options struct {
-	Verbose     bool
-	Company     string
-	Copyright   string
-	Description string
-	License     string
-	MailAddress string
-	ScriptName  string
-	Title       string
-	User        string
-	UserName    string
-	Lang        string
-}
 
 func init() {
 	log.SetFormatter(&log.TextFormatter{
@@ -40,11 +28,45 @@ func init() {
 	log.SetLevel(log.InfoLevel)
 }
 
-func Usage() {
+func Usage(lang string) string {
+	if lang == "" {
+		langs := templates.ListTemplateNames()
+		lang = fmt.Sprintf("[%s]", strings.Join(langs, "|"))
+	}
+	return fmt.Sprintf("USAGE:\n\n\t%s %s [<filename>] <options>\n\n", os.Args[0], lang)
+}
 
-	langs := templates.ListTemplateNames()
-	fmt.Printf("%s [%s] <options>\n", os.Args[0], strings.Join(langs, "|"))
+func WriteFile(tmpl *config.TemplateConfig, content string) error {
+	log.Debugf("WriteFile, start")
+	defer log.Debugf("WriteFile, end")
 
+	if config.TargetExists(tmpl.ScriptName) {
+		return fmt.Errorf("target already exists: %s", tmpl.ScriptName)
+	}
+
+	file, err := os.Create(tmpl.ScriptName)
+	defer file.Close()
+
+	obj := tmpl.GetItem(tmpl.Lang)
+	log.Debugf("language found: %s", obj.Name)
+	log.Debugf("mode: %o", obj.Mode)
+
+	_, err = file.WriteString(content)
+	if err != nil {
+		log.Errorf("file.WriteString: error: %s", err)
+		return err
+	} else {
+		log.Info("write file")
+	}
+
+	mode := os.FileMode(obj.Mode)
+	if err := os.Chmod(tmpl.ScriptName, mode); err != nil {
+		log.Error(err)
+		return err
+	} else {
+		log.Debugf("mode: %d\n", obj.Mode)
+	}
+	return nil
 }
 
 func main() {
@@ -52,7 +74,7 @@ func main() {
 	args := os.Args[1:]
 
 	if len(args) == 0 {
-		Usage()
+		fmt.Print(Usage(""))
 		return
 
 	}
@@ -75,68 +97,98 @@ func main() {
 	tmplfile, err := templates.GetTemplateFile(lang)
 	if err != nil {
 		fmt.Printf("unable to get template for %s: %s\n\n", lang, err)
-		Usage()
+		Usage("")
 		return
 	}
 
 	log.Debugf("template: %s", tmplfile)
 
-	options := &Options{}
-	options.Lang = lang
-
-	cfg := config.NewTemplateConfig(options.Lang)
+	cfg := config.NewTemplateConfig(lang)
 	cfg.Load()
 
 	f := flag.NewFlagSet("prompt", flag.ExitOnError)
 
-	f.BoolVar(&options.Verbose, "v", false, "Verbose logging")
+	f.BoolVar(&cfg.Verbose, "v", false, "Verbose logging")
 
-	f.StringVar(&options.Company, "company", cfg.Company, "Company name")
-	f.StringVar(&options.Company, "c", cfg.Company, "Company name")
+	f.StringVar(&cfg.Company, "company", cfg.Company, "Company name")
+	f.StringVar(&cfg.Company, "c", cfg.Company, "Company name")
 
-	f.StringVar(&options.Copyright, "copyright", cfg.Copyright, "Copyright holder")
+	f.StringVar(&cfg.Copyright, "copyright", cfg.Copyright, "Copyright holder")
 
-	f.StringVar(&options.Description, "description", "", "Script description")
-	f.StringVar(&options.Description, "d", "", "Script description")
+	f.StringVar(&cfg.Description, "description", "", "Script description")
+	f.StringVar(&cfg.Description, "d", "", "Script description")
 
-	f.StringVar(&options.License, "license", cfg.License, "License")
-	f.StringVar(&options.License, "l", cfg.License, "License")
+	f.StringVar(&cfg.License, "license", cfg.License, "License")
+	f.StringVar(&cfg.License, "l", cfg.License, "License")
 
-	f.StringVar(&options.MailAddress, "mailaddress", cfg.MailAdress, "mailaddress")
-	f.StringVar(&options.MailAddress, "m", cfg.MailAdress, "mailaddress")
+	f.StringVar(&cfg.MailAddress, "mailaddress", cfg.MailAdress, "mailaddress")
+	f.StringVar(&cfg.MailAddress, "m", cfg.MailAdress, "mailaddress")
 
-	f.StringVar(&options.ScriptName, "scriptname", "", "Script name")
-	f.StringVar(&options.ScriptName, "s", "", "Script name")
+	f.StringVar(&cfg.ScriptName, "scriptname", "", "Script name")
+	f.StringVar(&cfg.ScriptName, "s", "", "Script name")
 
-	f.StringVar(&options.Title, "title", "", "Title (of e.g. python class)")
-	f.StringVar(&options.Title, "t", "", "Title (of e.g. python class)")
+	f.StringVar(&cfg.Title, "title", "", "Title (of e.g. python class)")
+	f.StringVar(&cfg.Title, "t", "", "Title (of e.g. python class)")
 
-	f.StringVar(&options.UserName, "username", cfg.Username, "Users full name")
-	f.StringVar(&options.UserName, "u", cfg.Username, "Users full name")
+	f.StringVar(&cfg.UserName, "username", cfg.UserName, "Users full name")
+	f.StringVar(&cfg.UserName, "u", cfg.UserName, "Users full name")
 
-	f.StringVar(&options.User, "user", cfg.User, "User account name")
-	f.StringVar(&options.User, "U", cfg.User, "User account name")
+	f.StringVar(&cfg.User, "user", cfg.User, "User account name")
+	f.StringVar(&cfg.User, "U", cfg.User, "User account name")
 
 	f.Usage = func() {
-		fmt.Fprintf(os.Stderr, "USAGE:\n\n\t%s %s <options>\n\nOptions:\n\n", os.Args[0], options.Lang)
+		fmt.Fprintf(os.Stderr, "%s", Usage(cfg.Lang))
 		f.PrintDefaults()
 		fmt.Fprintf(os.Stderr, "\n\n")
 	}
 
 	f.Parse(args)
 
-	if len(options.ScriptName) != 0 {
+	if len(cfg.ScriptName) == 0 {
 		args := f.Args()
 		if len(args) > 0 {
-			options.ScriptName = args[0]
+			cfg.ScriptName = args[0]
 		}
 	}
+	if len(cfg.ScriptName) == 0 {
+		cfg.Stdout = true
+		cfg.ScriptName = "stdout"
+	}
 
-	if options.Verbose {
+	if cfg.Verbose {
 		log.SetLevel(log.DebugLevel)
 	}
 
-	for _, indx := range cfg.Object.Sections() {
-		fmt.Printf("section: %s\n", indx.Name())
+	// Setup description
+	cfg.Description = cfg.GetKeyAsString("description")
+
+	// get template content
+	templatestring, _ := templates.GetTemplateContent(cfg.Lang)
+	text_template, err := template.New("tmpl").Parse(templatestring)
+	if err != nil {
+		log.Error(err)
+		return
 	}
+
+	buf := new(bytes.Buffer)
+	err = text_template.Execute(buf, *cfg)
+
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	content := buf.String()
+
+	// Print to stdout if needed
+	if cfg.Stdout {
+		fmt.Print(content)
+		return
+	}
+
+	err = WriteFile(cfg, content)
+
+	if err != nil {
+		log.Error(err)
+	}
+
 }
